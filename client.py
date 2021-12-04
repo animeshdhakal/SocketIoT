e3e9c63d564a75da98690f003171812ae649d7a6
Animesh Dhakal <dhakalanimesh385@gmail.com>
2021-12-04 13:27:07 +0545

    SSL Fixed

--- client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect((socket.gethostname(), 1222))
-
-print(s.recv(1000000))
--- server/src/main/java/animesh/app/server/SSLHandlerProvider.java
+++ server/src/main/java/animesh/app/server/SSLHandlerProvider.java
@@ -2,47 +2,52 @@ package animesh.app.server;
 
 import java.io.InputStream;
 import java.security.KeyStore;
-
-import javax.net.ssl.KeyManager;
+import java.security.SecureRandom;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
-import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 import io.netty.handler.ssl.SslHandler;
 
 public class SSLHandlerProvider {
-    static String keystorePath = "/ssl/keystore.p12";
-    static String keystoretype = "PKCS12";
-    static String protocol = "TLS";
-    static String keystorePass = "changeit";
-    static String algorithm = "SunX509";
-    static SSLContext serverSSLContext = null;
+    private static final String jksFileName = "ssl/server.jks";
+    private static final String STOREPASS = "animesh";
+    private static final String KEYSTORETYPE = "jks";
+    private static final String PROTOCOL = "TLS";
+    public static SSLContext sslContext = null;
 
     static SslHandler getSslHandler() {
-        SSLEngine sslEngine = null;
-        if (serverSSLContext != null) {
-            sslEngine = serverSSLContext.createSSLEngine();
-            sslEngine.setUseClientMode(false);
-            sslEngine.setNeedClientAuth(false);
-
-            return new SslHandler(sslEngine);
+        if (sslContext != null) {
+            SSLEngine engine = sslContext.createSSLEngine();
+            engine.setUseClientMode(false);
+            return new SslHandler(engine);
         }
         return null;
-
     }
 
     static void init() {
         try {
-            serverSSLContext = SSLContext.getInstance(protocol);
-            KeyStore keystore = KeyStore.getInstance(keystoretype);
-            keystore.load(SSLHandlerProvider.class.getResourceAsStream(keystorePath), keystorePass.toCharArray());
-            KeyManagerFactory kmf = KeyManagerFactory.getInstance(algorithm);
-            kmf.init(keystore, keystorePass.toCharArray());
-            serverSSLContext.init(kmf.getKeyManagers(), null, null);
+
+            final ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            final InputStream inputStream = classloader.getResourceAsStream(jksFileName);
+
+            final KeyStore trustStore = KeyStore.getInstance(KEYSTORETYPE);
+            trustStore.load(inputStream, STOREPASS.toCharArray());
+
+            final KeyManagerFactory keyManagerFactory = KeyManagerFactory
+                    .getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(trustStore, STOREPASS.toCharArray());
+
+            final TrustManagerFactory trustManagerFactory = TrustManagerFactory
+                    .getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init(trustStore);
+
+            sslContext = SSLContext.getInstance(PROTOCOL);
+            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(),
+                    new SecureRandom());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
